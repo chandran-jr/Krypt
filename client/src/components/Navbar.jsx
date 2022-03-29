076e8c8ee82de2f857b36a1083ac359a9c3c8284
Govind B Chandran <govindchandran150@gmail.com>
2022-03-29 23:34:02 +0530

    Add files via upload

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,27 @@ import {HiMenuAlt4} from 'react-icons/hi';
 import {AiOutlineClose} from 'react-icons/ai';
 import logo from '../../images/logo.png';
 
+const NavBarItem = ({title,classProps}) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
+
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} className="w-32 cursor-pointer" alt="logo"/>
             </div>
+
+            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+            {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+        ))}
+            </ul>
+            
         </nav>
     );
 }
