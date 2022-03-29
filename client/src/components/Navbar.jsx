71fa05b1d3366b9afa22010b97105eb0689c7e8b
Govind B Chandran <govindchandran150@gmail.com>
2022-03-29 22:53:46 +0530

    Add files via upload

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+import logo from '../../images/logo.png';
+
 const Navbar = () => {
     return (
-        <h1>Krypt</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} className="w-32 cursor-pointer" alt="logo"/>
+            </div>
+        </nav>
     );
 }
 
