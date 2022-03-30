2e7bce96964d9dd5c9920a0a2b1d3ab9a92fbcbc
Govind B Chandran <govindchandran150@gmail.com>
2022-03-30 19:19:29 +0530

    Add files via upload

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,6 +21,9 @@ const Navbar = () => {
             {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
+            <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                Login
+            </li>
             </ul>
             
         </nav>
