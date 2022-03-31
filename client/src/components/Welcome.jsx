b4f2acec6b07ae14668a0a2145a9b1cc975f62c7
Govind B Chandran <govindchandran150@gmail.com>
2022-03-31 18:07:10 +0530

    Add files via upload

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -9,7 +9,7 @@ const Welcome = () => {
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
-                    <h1>Send Crypto <br/>Anywhere, Anytime!</h1>
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br/>Anywhere, Anytime!</h1>
                 </div>
             </div>
         </div>
