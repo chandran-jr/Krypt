c8d42718bffeb9a421a2115a0abf511342aaf367
Govind B Chandran <govindchandran150@gmail.com>
2022-03-31 17:11:51 +0530

    Add files via upload

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,18 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import {SiEthereum} from 'react-icons/si';
+import {BsInfoCircle} from 'react-icons/bs';
+
+import {Loader} from './'
+
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1>Send Crypto <br/>Anywhere, Anytime!</h1>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Welcome;
