bc70ed2b5a7710e8648400854c626a83fa7a1418
Govind B Chandran <govindchandran150@gmail.com>
2022-03-31 16:40:05 +0530

    Add files via upload

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import {HiMenuAlt4} from 'react-icons/hi';
 import {AiOutlineClose} from 'react-icons/ai';
 import logo from '../../images/logo.png';
+import React from 'react';
+import {useState} from 'react';
 
 const NavBarItem = ({title,classProps}) => {
     return (
@@ -12,7 +14,7 @@ const NavBarItem = ({title,classProps}) => {
 
 const Navbar = () => {
 
-    const [toggleMenu,setToggleMenu] = React.useState(false);
+    const [toggleMenu,setToggleMenu] = useState(false);
 
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
@@ -20,7 +22,7 @@ const Navbar = () => {
                 <img src={logo} className="w-32 cursor-pointer" alt="logo"/>
             </div>
 
-            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
             {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
@@ -29,7 +31,16 @@ const Navbar = () => {
             </li>
             </ul>
             <div className="flex-relative">
-
+                {toggleMenu ? <AiOutlineClose fontSize={28} className="text-white cursor-pointer md:hidden" onClick={() => setToggleMenu(false)} /> : <HiMenuAlt4 fontSize={28} className="text-white cursor-pointer md:hidden" onClick={() => setToggleMenu(true)} />}
+                {toggleMenu && 
+                (<ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism  text-white animate-slide-in">
+                <li className="text-xl w-full my-2">
+                    <AiOutlineClose className="cursor-pointer" onClick={() => setToggleMenu(false)}/>
+                </li>
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} classProps="my-2 text-lg" />
+        ))}
+                </ul>)}
             </div>
             
         </nav>
