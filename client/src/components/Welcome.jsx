ae036323a694c4203b67c3bc8d321504e9f6b0f1
Govind B Chandran <govindchandran150@gmail.com>
2022-03-31 19:33:49 +0530

    Add files via upload

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -16,7 +16,7 @@ const Welcome = () => {
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br/>Anywhere, Anytime!</h1>
-                    <p className="text-left mt-5 text-red-500 font-bold md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy & Sell Crypto here!!</p>
+                    <p className="text-left mt-5 text-green-500 font-bold md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy & Sell Crypto here!!</p>
                     <button type="button" className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] text-white font-bold" onClick={connectWallet}>
                         Connect Wallet
                     </button>
@@ -41,6 +41,9 @@ const Welcome = () => {
                         </div>
                     </div>
                 </div>
+
+
+
             </div>
         </div>
     );
