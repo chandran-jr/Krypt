496920ddaa3b91b10005e2298457cbbb33940167
Govind B Chandran <govindchandran150@gmail.com>
2022-03-31 18:27:30 +0530

    Add files via upload

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,11 +5,19 @@ import {BsInfoCircle} from 'react-icons/bs';
 import {Loader} from './'
 
 const Welcome = () => {
+    const connectWallet = () => {
+    
+    }
+
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br/>Anywhere, Anytime!</h1>
+                    <p className="text-left mt-5 text-red-500 font-bold md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy & Sell Crypto here!!</p>
+                    <button type="button" className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] text-white font-bold" onClick={connectWallet}>
+                        Connect Wallet
+                    </button>
                 </div>
             </div>
         </div>
