1d95d7739eef67564417beb74dd7bfe4b1d8db3c
Govind B Chandran <govindchandran150@gmail.com>
2022-04-01 11:13:56 +0530

    Add files via upload

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,68 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import {SiEthereum} from 'react-icons/si';
+import {BsInfoCircle} from 'react-icons/bs';
+
+import {Loader} from './'
+
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[2px] border-white-400 text-sm font-bold text-white";
+
+const Welcome = () => {
+    const connectWallet = () => {
+    
+    }
+
+    return (
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br/>Anywhere, Anytime!</h1>
+                    <p className="text-left mt-5 text-green-500 font-bold md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy, Sell & hold crypto here!!</p>
+                    <button type="button" className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] text-white font-bold" onClick={connectWallet}>
+                        Connect Wallet
+                    </button>
+                    <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+                        <div className={`rounded-tl-2xl ${commonStyles}`}>
+                            Reliable
+                        </div>
+                        <div className={commonStyles}>
+                            Secure
+                        </div>
+                        <div className={`rounded-tr-2xl ${commonStyles}`}>
+                            Fast
+                        </div>
+                        <div className={`rounded-bl-2xl ${commonStyles}`}>
+                            Web 3.0
+                        </div>
+                        <div className={`${commonStyles}`}>
+                            Ethereum
+                        </div>
+                        <div className={`rounded-br-2xl ${commonStyles}`}>
+                            Low fees
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-1 flex-col items-center justify-center w-full md:mt-0 mt-10">
+                    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    <SiEthereum fontSize={21} color="white"/>
+                                </div>
+                                <BsInfoCircle className="cursor-pointer" fontSize={17} color="white"/>
+                            </div>
+                            <div>
+                                <p className="text-white font-light text-sm">
+                                    0xabggskdhsl....hshahksjja
+                                </p>
+                            </div>
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
+export default Welcome;
