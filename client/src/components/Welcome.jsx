6478305a8613a3aef1986082220d2a3cdc66be8a
Govind B Chandran <govindchandran150@gmail.com>
2022-04-01 19:50:09 +0530

    Add files via upload

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -15,7 +15,6 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
     onChange={(e) => handleChange(e,name)}
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
     />
-
 )
 
 const handleChange = (e) => {
@@ -26,6 +25,13 @@ const Welcome = () => {
     const connectWallet = () => {
     
     }
+
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
+    
+        e.preventDefault();
+        
+      };
 
     return (
         <div className="flex w-full justify-center items-center">
@@ -78,13 +84,25 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism border-white border-2">
+                <div className="p-5 sm:w-96 w-full flex flex-col justify-start mt-2 items-center blue-glassmorphism border-white border-2">
                     <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
                     <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
                     <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={handleChange} />
                     <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
                     <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+                    {false
+              ? <Loader />
+              : (
+                <button
+                  type="button"
+                  onClick={handleSubmit}
+                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                >
+                  Send now
+                </button>
+              )}
                 </div>
                 </div>
 
