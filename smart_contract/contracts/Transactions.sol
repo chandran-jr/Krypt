23a11ea7d24ba829ca68e6503223d5a902e97074
Govind B Chandran <govindchandran150@gmail.com>
2022-04-02 15:13:09 +0530

    Add files via upload

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
+
+contract Transactions {
+
+    uint256 transactionCounter;
+
+    event Transfer (address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+}
