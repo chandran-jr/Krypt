bc00f3031f4835d22bc11d071869bfbaf71ca81e
Govind B Chandran <govindchandran150@gmail.com>
2022-04-02 14:41:39 +0530

    Add files via upload

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -35,8 +35,8 @@ const Welcome = () => {
 
     return (
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br/>Anywhere, Anytime!</h1>
                     <p className="text-left mt-5 text-green-500 font-bold md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy, Sell & hold crypto here!!</p>
                     <button type="button" className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] text-white font-bold" onClick={connectWallet}>
@@ -64,7 +64,7 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-1 flex-col items-center justify-center w-full md:mt-0 mt-10">
+                <div className="flex flex-1 flex-col items-center justify-center w-full mf:mt-0 mt-10">
                     <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
