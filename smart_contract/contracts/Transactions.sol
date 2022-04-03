800e793e4ea6b73acefff4a5c9534fc88d93cad5
Govind B Chandran <govindchandran150@gmail.com>
2022-04-03 10:18:28 +0530

    Add files via upload

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -7,4 +7,27 @@ contract Transactions {
     uint256 transactionCounter;
 
     event Transfer (address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain() public {
+
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        // return all transactions
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        //return Transaction Count
+    }
 }
