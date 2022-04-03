1b49863f2e07c760c65b167acf1c50e38ffa0f95
Govind B Chandran <govindchandran150@gmail.com>
2022-04-03 13:34:58 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,10 +4,10 @@ require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
-  network: {
+  networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/SGf4PqnjzMkShZFtY6QHv2BFOAbqmmuf',
-      accounts: ['Your Private Key from Metamask']
+      accounts: ['Your own Private Key from Metamask']
     }
   }
 }
