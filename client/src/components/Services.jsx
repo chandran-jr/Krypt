d2ee8889f7046692c35e37efdb9adb428762ba38
Govind B Chandran <govindchandran150@gmail.com>
2022-04-07 22:45:41 +0530

    Add files via upload

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -6,7 +6,11 @@ import {RiHeart2Fill} from 'react-icons/ri';
 const Services = () => {
     return (
         <div className="flex w-full justify-center items-center gradient-bg-services">
-
+            <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+                <div className="flex-1 flex flex-col justify-start items-start">
+                    <h1 className="text-white">Services We <br/>Continue to Improve</h1>
+                </div>
+            </div>
         </div>
     );
 }
