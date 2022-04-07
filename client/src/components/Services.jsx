13bc6af4e52b08849ad9dd79d32b920b36629045
Govind B Chandran <govindchandran150@gmail.com>
2022-04-07 22:56:17 +0530

    Add files via upload

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -2,14 +2,28 @@ import {BsShieldFillCheck} from 'react-icons/bs';
 import {BiSearchAlt} from 'react-icons/bi';
 import {RiHeart2Fill} from 'react-icons/ri';
 
+const ServiceCard = () => (
+<div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+    <h1>Service Card</h1>
+</div>
+)
 
 const Services = () => {
     return (
         <div className="flex w-full justify-center items-center gradient-bg-services">
             <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
                 <div className="flex-1 flex flex-col justify-start items-start">
-                    <h1 className="text-white">Services We <br/>Continue to Improve</h1>
+                    <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">Services that we <br/>Continue to Improve</h1>
                 </div>
+            </div>
+
+            <div className="flex-1 flex flex-col justify-start items-center">
+                <ServiceCard
+                    color="bg-[#2952e3]"
+                    title="Security Guaranteed"
+                    icon={<BsShieldFillCheck fontSize={21} className="text-white"/>}
+                    subTitle="Security is always our proority, we maintain privacy and quality"
+                />
             </div>
         </div>
     );
