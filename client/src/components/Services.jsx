2702e14bec8bd1f7fd6ee2e50ba43ec2419b24f7
Govind B Chandran <govindchandran150@gmail.com>
2022-04-07 22:32:14 +0530

    Add files via upload

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,6 +1,13 @@
+import {BsShieldFillCheck} from 'react-icons/bs';
+import {BiSearchAlt} from 'react-icons/bi';
+import {RiHeart2Fill} from 'react-icons/ri';
+
+
 const Services = () => {
     return (
-        <h1>Services</h1>
+        <div className="flex w-full justify-center items-center gradient-bg-services">
+
+        </div>
     );
 }
 
