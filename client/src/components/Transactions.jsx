d9067d8680b5ddb97b813a74eb8366ef4cc5714b
Govind B Chandran <govindchandran150@gmail.com>
2022-04-08 17:42:06 +0530

    Add files via upload

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import React from "react";
+import {TransactionContext} from "../context/TransactionContext";
+import dummyData from "../utils/dummyData";
+
+
+
 const Transactions = () => {
+    const {currentAccount} = useContext(TransactionContext);
+
     return (
-        <h1>Transactions</h1>
+        <div className="flex w-ful justify-center items-center 2xl:px-20 gradient-bg-transactions">
+            <div className="flex flex-col md:p-12 py-12 px-4">
+                {currentAccount ? (<h3 className=" text-white text-3xl text-center my-2">Latest Transactions</h3>) : (<h3 className=" text-white text-3xl text-center my-2">Connect Wallet</h3>)}
+            </div>
+        </div>
     );
 }
 
